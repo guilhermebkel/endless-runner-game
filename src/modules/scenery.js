3ed854f8ea4fe1f2a955d0d284120a1085569a8e
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-22 19:46:46 -0300

    feat(*): add movement to character

---
+++ src/modules/scenery.js
@@ -0,0 +1,27 @@
+class Scenery {
+  constructor(picture, speed) {
+    this.picture = picture
+    this.speed = speed
+    
+    this.x1 = 0
+    this.x2 = width
+  }
+
+  show() {
+    image(this.picture, this.x1, 0, width, height)
+    image(this.picture, this.x2, 0, width, height)
+  }
+
+  move() {
+    this.x1 -= this.speed
+    this.x2 -= this.speed
+
+    if (this.x1 < -width) {
+      this.x1 = width
+    }
+
+    if (this.x2 < -width) {
+      this.x2 = width
+    }
+  }
+}
--- src/modules/sketch.js
+++ src/modules/sketch.js
@@ -1,16 +1,31 @@
 let sceneryPicture
 let characterPicture
 
+let scenery
+let character
+
+let gameSound
+
 function preload() {
   sceneryPicture = loadImage("src/assets/images/scenery/forest.png")
   characterPicture = loadImage("src/assets/images/character/running.png")
+  gameSound = loadSound("src/assets/songs/soundtrack.mp3")
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
+
+  scenery = new Scenery(sceneryPicture, 3)
+  character = new Character(characterPicture)
+
+  gameSound.loop()
+
+  frameRate(40)
 }
 
 function draw() {
-  background(sceneryPicture)
-  image(characterPicture, 0, height - 135, 110, 135, 0, 0, 220, 270)
+  scenery.show()
+  scenery.move()
+
+  character.show()
 }
