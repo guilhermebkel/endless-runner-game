d3fec9b4007e17e3b4a56d8123df2bfa802fe1ab
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-22 18:58:23 -0300

    feat(icon): add icon

--- src/modules/sketch.js
+++ src/modules/sketch.js
@@ -12,5 +12,5 @@ function setup() {
 
 function draw() {
   background(sceneryPicture)
-  image(characterPicture, 0, 0, 220, 270, 0, 0)
+  image(characterPicture, 0, height - 135, 110, 135, 0, 0, 220, 270)
 }
